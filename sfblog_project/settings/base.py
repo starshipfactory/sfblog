5e451dba0457ed8d0e9db853183e9d950671dc98
Juliane Clausen <git@juliane-clausen.de>
2014-05-10 19:23:36 +0200

    comment deleted, staying on sqlite

--- sfblog_project/settings/base.py
+++ sfblog_project/settings/base.py
@@ -96,16 +96,6 @@ DATABASES = {
     }
 }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE':'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'sfblog',
-#        'USER': 'sfblog',
-#        'PASSWORD': 'test',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-#    }
-#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
