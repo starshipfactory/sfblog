e63bf589610554672e75fec06d605af271527e63
Caoimhe Chaos <caoimhechaos@protonmail.com>
2014-05-11 02:41:37 +0200

    Add a prod settings module which points to PostgreSQL.

--- sfblog_project/settings/prod.py
+++ sfblog_project/settings/prod.py
@@ -1,3 +1,17 @@
 from .base import *  # NOQA
+from os import environ
+
+environ.setdefault('SFBLOG_CONFIG_PATH', '/etc/sfblog')
 
 DEBUG = False
+
+DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'sfblog',
+                'USER': 'sfblog',
+                'PASSWORD': open(environ.get('SFBLOG_CONFIG_PATH') + '/pgpassword').read().strip(),
+                'HOST': open(environ.get('SFBLOG_CONFIG_PATH') + '/pgserver').read().strip(),
+                'PORT': open(environ.get('SFBLOG_CONFIG_PATH') + '/pgport').read().strip(),
+        }
+}
