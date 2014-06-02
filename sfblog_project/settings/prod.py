0e9d93b7e0998df6f5299bb7666adbcdedb5de28
Caoimhe Chaos <caoimhechaos@protonmail.com>
2014-06-02 23:16:02 +0200

    Whitelist the official names for the starship factory blog.

--- sfblog_project/settings/prod.py
+++ sfblog_project/settings/prod.py
@@ -3,6 +3,36 @@ from os import environ
 
 environ.setdefault('SFBLOG_CONFIG_PATH', '/etc/sfblog')
 
+ALLOWED_HOSTS = [
+		"blog.starship-factory.ch",
+		"blog.starship-factory.com",
+		"blog.starship-factory.de",
+		"blog.starship-factory.eu",
+		"blog.starship-factory.org",
+		"blog.starshipfactory.ch",
+		"blog.starshipfactory.de",
+		"blog.starshipfactory.eu",
+		"blog.starshipfactory.org",
+		"starship-factory.ch",
+		"starship-factory.com",
+		"starship-factory.de",
+		"starship-factory.eu",
+		"starship-factory.org",
+		"starshipfactory.ch",
+		"starshipfactory.de",
+		"starshipfactory.eu",
+		"starshipfactory.org",
+		"www.starship-factory.ch",
+		"www.starship-factory.com",
+		"www.starship-factory.de",
+		"www.starship-factory.eu",
+		"www.starship-factory.org",
+		"www.starshipfactory.ch",
+		"www.starshipfactory.de",
+		"www.starshipfactory.eu",
+		"www.starshipfactory.org",
+		]
+
 DEBUG = False
 
 DATABASES = {
