9934950b6619fc803961a4919c440ee123eb5363
Alexander Clausen <alex@gc-web.de>
2014-07-07 23:58:33 +0200

    hopefully make the facebook opengraph debugger happy
    the main image is shown in facebook as-is, but the debugger wants
    to have a "real", absolute, URL
    
    see https://developers.facebook.com/tools/debug/

--- sfblog_project/settings/base.py
+++ sfblog_project/settings/base.py
@@ -47,6 +47,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.core.context_processors.static',
     'zinnia.context_processors.version',
+    'core.context_processors.site',
 )
 
 # Application definition
