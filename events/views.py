655b407780f4b16981ac19166f0c71f6e4c56e5f
Alexander Clausen <alex@gc-web.de>
2015-04-07 20:32:59 +0200

    move events around in the sidebar

--- events/views.py
+++ events/views.py
@@ -8,7 +8,15 @@ class EventList(ListView):
     template_name = "events/event_list.html"
     context_object_name = "events"
 
+    def get_calendar(self):
+        return Calendar.objects.get(slug="default")
+
+    def get_context_data(self, **kwargs):
+        ctx = super(EventList, self).get_context_data(**kwargs)
+        ctx['cal'] = self.get_calendar()
+        return ctx
+
     def get_queryset(self):
-        cal = Calendar.objects.get(slug="default")
+        cal = self.get_calendar()
         occurrences = cal.occurrences_after(now())
         return islice(occurrences, 25)
