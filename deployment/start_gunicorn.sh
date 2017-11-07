d0467dc09421c1ca2847480d35b12e505e4cb267
Fabian Thoma <fabian@elementaryos.org>
2017-11-07 21:50:19 +0100

    Erhöhe Workeranzahl für gleichzeitiges Zugreifen

--- deployment/start_gunicorn.sh
+++ deployment/start_gunicorn.sh
@@ -3,4 +3,4 @@ export PYTHONPATH=/sfblog
 /usr/local/bin/django-admin.py collectstatic --noinput
 /usr/local/bin/django-admin.py migrate
 /usr/local/bin/django-admin.py simplecms_init
-/usr/local/bin/gunicorn --pythonpath /sfblog sfblog_project.wsgi:application -b 0.0.0.0:8000
+/usr/local/bin/gunicorn -w 4 --pythonpath /sfblog sfblog_project.wsgi:application -b 0.0.0.0:8000
